0cf09ac3964b79e47961cc9ae609a0359ea8d548
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-12-28 18:20:26 +0000

    app enclosure ready
    The entire app will be enclosed in an object which will be returned by a function.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import "./styles.css";
 
 // API : https://newsapi.org/
 // API key: 2043fa143b224d2b8f1057943e2557f7
+
+function buildNewsApp() {
+  const newsApp = {};
+  return newsApp;
+}
+
+const newsApp = buildNewsApp();
