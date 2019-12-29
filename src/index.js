717c81372150f10d40aee042c4361601f83c9e4d
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-12-29 18:08:59 +0000

    added JS to the mix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./styles.css";
 
-// API : https://newsapi.org/
+// API : https://newsapi.org/v2/top-headlines&apiKey=2043fa143b224d2b8f1057943e2557f7
 // API key: 2043fa143b224d2b8f1057943e2557f7
 
 function buildNewsApp() {
+  const newsArticlesDiv = document.querySelectorAll(".news-articles");
+  const filtersDiv = document.querySelector(".filters");
+  console.log(newsArticlesDiv, filtersDiv);
   const newsApp = {};
   return newsApp;
 }
