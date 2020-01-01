f212b3b8e0d29e494423da20ab405a73366ef7ad
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-01 12:47:26 +0530

    using fetch instead of Axios for api calls

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import "./styles.css";
-import axios from "axios";
 // API : https://newsapi.org/v2/top-headlines&apiKey=2043fa143b224d2b8f1057943e2557f7
 // API key: 2043fa143b224d2b8f1057943e2557f7
 
 function buildNewsApp() {
   const newsArticlesDiv = document.querySelectorAll(".news-articles");
   const filtersDiv = document.querySelector(".filters");
-  const newsApp = {};
+  const newsApp = {
+    getData: function() {
+      fetch(
+        "https://newsapi.org/v2/everything?q=viratkohli&apiKey=2043fa143b224d2b8f1057943e2557f7"
+      )
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data);
+        });
+    }
+  };
+  newsApp.getData();
   return newsApp;
 }
 
 const newsApp = buildNewsApp();
-
-async function getNewsData() {
-  let newsPromise = new Promise((resolve, reject) => {
-    return resolve();
-  });
-  newsPromise.then(data => {});
-}
