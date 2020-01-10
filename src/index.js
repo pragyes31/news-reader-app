c022463bc101cc89951170de0a4b548df7bdbf65
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-11 01:23:19 +0530

    Refactor code a little

--- src/index.js
+++ src/index.js
@@ -15,6 +15,54 @@ function buildNewsApp() {
   const sortBy = document.querySelector("#sort-by");
   const newsAPIKey = "apiKey=2043fa143b224d2b8f1057943e2557f7";
   const newsApp = {
+    createNewsArticleDiv: function(
+      header,
+      timestamp,
+      description,
+      articleLink
+    ) {
+      let newsArticleDiv = document.createElement("div");
+      let newsHeaderDiv = document.createElement("header");
+      let publishDateDiv = document.createElement("div");
+      let newsDescDiv = document.createElement("div");
+      let readMoreDiv = document.createElement("div");
+      let readMoreLink = document.createElement("a");
+      newsArticleDiv.className = "news-article";
+      newsHeaderDiv.className = "news-title";
+      publishDateDiv.className = "date-of-publish";
+      newsDescDiv.className = "news-description";
+      readMoreDiv.className = "read-more";
+      newsArticlesDiv.appendChild(newsArticleDiv);
+      newsArticleDiv.appendChild(newsHeaderDiv);
+      newsArticleDiv.appendChild(publishDateDiv);
+      newsArticleDiv.appendChild(newsDescDiv);
+      newsArticleDiv.appendChild(readMoreDiv);
+      readMoreDiv.appendChild(readMoreLink);
+      newsHeaderDiv.innerHTML = header;
+      publishDateDiv.innerHTML = timestamp;
+      newsDescDiv.innerHTML = description;
+      readMoreLink.innerHTML = "Read more";
+      readMoreLink.href = articleLink;
+      readMoreLink.setAttribute("target", "_blank");
+    },
+    populateNewsArticles: function(newsUrl) {
+      fetch(newsUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          data.articles.forEach(article => {
+            let jsonDate = article.publishedAt;
+            let date = new Date(jsonDate);
+            newsApp.createNewsArticleDiv(
+              article.title,
+              date,
+              article.description,
+              article.url
+            );
+          });
+        });
+    },
     switchTabs: function(e) {
       newsCategory.forEach(category => {
         category.classList.remove("active-news-category");
@@ -69,78 +117,6 @@ function buildNewsApp() {
       let newsUrl = `${topHeadlinesEntryPoint}?q=${selectedCountry}${selectedCategory}&${newsAPIKey}`;
       newsArticlesDiv.innerHTML = "";
       newsApp.populateNewsArticles(newsUrl);
-    },
-    filteredNews: function(e) {
-      e.preventDefault();
-      let selectedCountry =
-        countrySelector.options[countrySelector.selectedIndex].value === ""
-          ? ""
-          : `&country=${
-              countrySelector.options[countrySelector.selectedIndex].value
-            }`;
-      let selectedCategory =
-        categorySelector.options[categorySelector.selectedIndex].value === ""
-          ? ""
-          : `&category=${
-              categorySelector.options[categorySelector.selectedIndex].value
-            }`;
-      let newsAPIEntryPoint = "https://newsapi.org/v2/top-headlines";
-      const newsAPIKey = "apiKey=2043fa143b224d2b8f1057943e2557f7";
-      let newsUrl = `${newsAPIEntryPoint}?q=${selectedCountry}${selectedCategory}&${newsAPIKey}`;
-      newsArticlesDiv.innerHTML = "";
-      newsApp.populateNewsArticles(newsUrl);
-      //console.log(keyword, selectedCountry, selectedCategory);
-    },
-    createNewsArticleDiv: function(
-      header,
-      timestamp,
-      description,
-      articleLink
-    ) {
-      let newsArticleDiv = document.createElement("div");
-      let newsHeaderDiv = document.createElement("header");
-      let publishDateDiv = document.createElement("div");
-      let newsDescDiv = document.createElement("div");
-      let readMoreDiv = document.createElement("div");
-      let readMoreLink = document.createElement("a");
-      newsArticleDiv.className = "news-article";
-      newsHeaderDiv.className = "news-title";
-      publishDateDiv.className = "date-of-publish";
-      newsDescDiv.className = "news-description";
-      readMoreDiv.className = "read-more";
-      newsArticlesDiv.appendChild(newsArticleDiv);
-      newsArticleDiv.appendChild(newsHeaderDiv);
-      newsArticleDiv.appendChild(publishDateDiv);
-      newsArticleDiv.appendChild(newsDescDiv);
-      newsArticleDiv.appendChild(readMoreDiv);
-      readMoreDiv.appendChild(readMoreLink);
-      newsHeaderDiv.innerHTML = header;
-      publishDateDiv.innerHTML = timestamp;
-      newsDescDiv.innerHTML = description;
-      readMoreLink.innerHTML = "Read more";
-      readMoreLink.href = articleLink;
-      readMoreLink.setAttribute("target", "_blank");
-    },
-    populateNewsArticles: function(newsUrl) {
-      fetch(newsUrl)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          //console.log(data);
-          data.articles.forEach(article => {
-            //console.log("boom");
-            let jsonDate = article.publishedAt;
-            let date = new Date(jsonDate);
-            //console.log(jsonDate, date);
-            newsApp.createNewsArticleDiv(
-              article.title,
-              date,
-              article.description,
-              article.url
-            );
-          });
-        });
     }
   };
   newsApp.populateNewsArticles(
