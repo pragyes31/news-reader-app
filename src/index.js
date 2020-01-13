55ea2910643b5c12094ddbba019d62b17568e4d7
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-13 17:08:02 +0530

    Color theme defined for the app

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ function buildNewsApp() {
     }
   };
   newsApp.populateNewsArticles(
-    "https://newsapi.org/v2/everything?q=javascript&apiKey=2043fa143b224d2b8f1057943e2557f7 "
+    "https://newsapi.org/v2/top-headlines?country=in&apiKey=2043fa143b224d2b8f1057943e2557f7 "
   );
   getEverythingBtn.addEventListener("click", newsApp.getEverythingNews);
   getTopHeadlinesBtn.addEventListener("click", newsApp.getTopHeadlines);
